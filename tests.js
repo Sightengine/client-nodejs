7c64403ce36c15ead637ee14c52cac00c02e26ed
agrandiere <agrandiere@intechinfo.fr>
2017-07-24 19:10:18 +0200

    change version in package.json and improve tests

--- tests.js
+++ tests.js
@@ -1,75 +1,74 @@
 var sightengine = require('./sightengine')('1234','test');
 var assert = require('assert');
 var fs = require('fs');
+var path = require('path');
 
 describe('test feedback', function() {
-  it('should return success', function() {
-    return sightengine.feedback('nudity', 'safe', 'https://sightengine.com/assets/img/examples/example5.jpg').then((result) => {
-      assert.equal('success', result.status);
+  it('should return success', function(done) {
+    sightengine.feedback('nudity', 'safe', 'https://sightengine.com/assets/img/examples/example5.jpg').then((result) => {
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return error', function() {
-    return sightengine.feedback('nudity', 'safe', 'https://sightengine.com/assets/img/examples/example99999.jpg').then((result) => {
-      assert.equal('failure', result.status);
-      assert.equal('media_error', result.error.type);
+  it('should return error', function(done) {
+    sightengine.feedback('nudity', 'safe', 'https://sightengine.com/assets/img/examples/example99999.jpg').then((result) => {
+      done(assert.equal('failure', result.status))
     })
   });
 });
 
 describe('test image moderation', function() {
-  it('should return success', function() {
+  it('should return success', function(done) {
     sightengine.check(['nudity', 'type', 'properties','wad','face', 'celebrities']).set_url('https://sightengine.com/assets/img/examples/example5.jpg').then(function(result) {
-      assert.equal('success', result.status);
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return success', function() {
+  it('should return success', function(done) {
     sightengine.check(['nudity']).set_url('https://sightengine.com/assets/img/examples/example5.jpg').then(function(result) {
-      assert.equal('success', result.status);
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return success', function() {
-    sightengine.check(['nudity', 'type', 'properties','wad','face', 'celebrities']).set_file('/assets/image.jpg').then(function(result) {
-      assert.equal('success', result.status);
+  it('should return success', function(done) {
+    sightengine.check(['nudity', 'type', 'properties','wad','face', 'celebrities']).set_file(path.resolve(__dirname, 'assets', 'image.jpg')).then(function(result) {
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return success', function() {
-    sightengine.check(['nudity']).set_file('/assets/image.jpg').then(function(result) {
-      assert.equal('success', result.status);
+  it('should return success', function(done) {
+    sightengine.check(['celebrities']).set_file(path.resolve(__dirname, 'assets', 'image.jpg')).then(function(result) {
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return success', function() {
-    var binaryImage = fs.createReadStream('/assets/image.jpg');
+  it('should return success', function(done) {
+    var binaryImage = fs.createReadStream(path.resolve(__dirname, 'assets', 'image.jpg'));
 
     sightengine.check(['nudity']).set_bytes(binaryImage).then(function(result) {
-      assert.equal('success', result.status);
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return success', function() {
-    var binaryImage = fs.createReadStream('/assets/image.jpg');
+  it('should return success', function(done) {
+    var binaryImage = fs.createReadStream(path.resolve(__dirname, 'assets', 'image.jpg'));
 
     sightengine.check(['nudity', 'type', 'properties','wad','face', 'celebrities']).set_bytes(binaryImage).then(function(result) {
-      assert.equal('success', result.status);
+      done(assert.equal('success', result.status))
     })
   });
 
-  it('should return error', function() {
+  it('should return error', function(done) {
     sightengine.check(['nudity', 'type', 'properties','wad','face']).set_url('https://sightengine.com/assets/img/examples/example99999.jpg').then(function(result) {
-      assert.equal('failure', result.status);
-      assert.equal('media_error', result.error.type);
+      done(assert.equal('failure', result.status))
     })
   });
 });
 
 describe('test video moderation', function() {
-  it('should return success', function() {
+  it('should return success', function(done) {
     sightengine.check(['nudity', 'type', 'properties','wad','face']).video('http://www.quirksmode.org/html5/videos/big_buck_bunny.webm', 'http://requestb.in/1d097l71').then(function(result) {
-      assert.equal('success', result.status);
+      done(assert.equal('success', result.status))
     })
   });
 });
